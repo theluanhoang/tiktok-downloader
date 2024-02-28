84716c44944036c303ce2675c4af65b922b30cef
theluanhoang <hoangtheluan2016@gmail.com>
2024-02-28 08:23:47 +0700

    fix: fetch file

--- src/api.js
+++ src/api.js
@@ -22,18 +22,42 @@ export const tiktokVideoDownloader = async (url, hd) => {
     }
 }
 
+// export const fetchFile = (url) => {
+//     fetch(url).then(res => res.blob()).then(file => {
+//         let tempUrl = URL.createObjectURL(file);
+//         const aTag = document.createElement("a");
+//         aTag.href = tempUrl;
+//         // aTag.download = url.replace(/^.*[\\\/]/, '');
+//         aTag.download = url.replace(/^.*[\\\/]/, '');
+//         document.body.appendChild(aTag);
+//         aTag.click();
+//         URL.revokeObjectURL(tempUrl);
+//         aTag.remove();
+//     }).catch(() => {
+//         throw new Error("Failed to download file!");
+//     });
+// }
+
 export const fetchFile = (url) => {
-    fetch(url).then(res => res.blob()).then(file => {
-        let tempUrl = URL.createObjectURL(file);
-        const aTag = document.createElement("a");
-        aTag.href = tempUrl;
-        aTag.download = url.replace(/^.*[\\\/]/, '');
-        document.body.appendChild(aTag);
-        aTag.click();
-        URL.revokeObjectURL(tempUrl);
-        aTag.remove();
-    }).catch(() => {
-        throw new Error("Failed to download file!");
-    });
+    fetch(url)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return res.blob();
+        })
+        .then(file => {
+            const tempUrl = URL.createObjectURL(file);
+            const aTag = document.createElement("a");
+            aTag.href = tempUrl;
+            aTag.setAttribute("download", ""); // Thiết lập thuộc tính download để tải xuống
+            document.body.appendChild(aTag);
+            aTag.click();
+            URL.revokeObjectURL(tempUrl);
+            aTag.remove();
+        })
+        .catch(() => {
+            throw new Error("Failed to download file!");
+        });
 }
 
